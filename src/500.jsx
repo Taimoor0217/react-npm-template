bd57973e33fc28ea4bfdd72aa6c6d0188cddb9e5
taimoor <taimoor.ali@conradlabs.com>
2020-09-05 17:33:47 +0500

    :wrench: add weback configs and setup files

---
+++ src/500.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function Error(props){
+    return <h1>Hello</h1>
+}
---
+++ test/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Error from "../src/500";
+
+export default function App(){
+    return(
+        <Error />
+    )
+}
